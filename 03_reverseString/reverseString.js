341dc68c99fe02e1d7b6fd8a6479be28ae2a8dba
siddharth-stn <sidarjunstn.sp@gmail.com>
2022-03-07 17:16:42 +0530

    All tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let finalString = '';
+    for (i = string.length - 1; i >= 0; i--) {
+        finalString += string[i];
+    }
+    return finalString;
 };
 
 // Do not edit below this line
