564e54ff199985d57943aabfac6c2d2f88d691a9
siddharth-stn <sidarjunstn.sp@gmail.com>
2022-03-07 17:08:10 +0530

    All tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR"; 
+    let finalString = '';
+    for (i = 0; i < num; i++) {
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
