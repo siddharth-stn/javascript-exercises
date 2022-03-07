349ae0db4164cb1d3152c30101445ddcde0abdbd
siddharth-stn <sidarjunstn.sp@gmail.com>
2022-03-07 18:12:03 +0530

    All tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elem) {
+    let index;
+    for (i = 0; i < elem.length; i++) {
+        index = array.indexOf(elem[i]);
+        if (index >= 0) array.splice(index, 1);
+    }
+    return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
