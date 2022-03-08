4028550843aec5137538f01f7bdb69572c48c87b
siddharth-stn <sidarjunstn.sp@gmail.com>
2022-03-08 16:16:30 +0530

    All tests completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempInFahrenheit) {
+  let tempInCelsius = 0;
+  tempInCelsius = Number(((tempInFahrenheit - 32) * (5/9)).toFixed(1));
+  return tempInCelsius;
 };
 
-const ctof = function() {
-
+const ctof = function(tempInCelsius) {
+  let tempInFahrenheit = 0;
+  tempInFahrenheit = Number((tempInCelsius * (9/5) + 32).toFixed(1));
+  return tempInFahrenheit;
 };
 
 // Do not edit below this line
